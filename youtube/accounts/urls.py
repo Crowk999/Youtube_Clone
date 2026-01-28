9b135debfec470d83ffe01c4e2d08c1af2498ad3
Adhrit Sigdel <adhritsigdel@gmail.com>
2026-01-28 19:54:07 +0545

    Progress

---
+++ youtube/accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+
+    path("login/", auth_views.LoginView.as_view(
+        template_name = "accounts/login.html"
+    ), name="login"),
+
+    path("logout/", auth_views.LoginView(
+        template_name="accounts/logout.html"), name="logout"
+    )
+
+]
